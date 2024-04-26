ef5d13ea7f70820db46de2ea8cb6fd2e6482eb4f
Duke Nyamongo (Lester) <dukelester4@gmail.com>
2024-04-27 02:28:20 +0300

    Updated the add expense

--- expenses/urls.py
+++ expenses/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='expenses'),
-    path('add/', views.add_expense, name='add_expense'),
+    path('add_expense', views.add_expense, name='add_expense'),
 ]

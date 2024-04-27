e899da9fea7a05c86a1d8ca6fa83cd68afd6f2dd
Duke Nyamongo (Lester) <dukelester4@gmail.com>
2024-04-27 14:21:44 +0300

    Added username validation

--- static/js/register.js
+++ static/js/register.js
@@ -1,6 +1,8 @@
 
 const usernameField = document.querySelector('#usernameField');
-
+const invalidFeedBackArea = document.querySelector('.invalid_feedback');
+usernameField.classList.remove('is-invalid');
+invalidFeedBackArea.style.display = 'none';
 // add eventy listener - keyup
 usernameField.addEventListener('keyup', (e) => {
     usernameValue = e.target.value;
@@ -10,6 +12,11 @@ usernameField.addEventListener('keyup', (e) => {
             body: JSON.stringify({ username: usernameValue }), method: 'POST'
         }).then(res => res.json()).then(data => {
             console.log(data);
+            if (data.username_error){
+                usernameField.classList.add('is-invalid');
+                invalidFeedBackArea.style.display = 'block';
+                invalidFeedBackArea.innerHTML = `<p>${data.username_error}</p>`
+            }
         });
     }
 });

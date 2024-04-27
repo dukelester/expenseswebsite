e9486ac1ef0b2baafc4dbb7537a9e64892341971
Duke Nyamongo (Lester) <dukelester4@gmail.com>
2024-04-27 12:25:11 +0300

    Added the validation for username

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-
+from django.views.decorators.csrf import csrf_exempt
 
 from . import views
 
 urlpatterns = [
     path('register', views.register, name='register'),
-    path('validate_username', views.UsernameValidationView.as_view(), name='validate_username'),
+    path('validate_username', csrf_exempt(views.UsernameValidationView.as_view()), name='validate_username'),
     path('login', views.login, name='login'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import JsonResponse
+from django.contrib.auth.models import User
 import json
 
 # Create your views here.
@@ -11,8 +12,12 @@ class UsernameValidationView(View):
         username = data['username']
         if not str(username).isalnum():
             return JsonResponse({
-                'username_error': 'username should only contain alphanumeric!'
-            })
+                'username_error': 'username should only contain alphanumeric!',
+            }, status=400)
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({
+                'username_error': 'Invalid username, choose another username'
+                }, status=409)
         return JsonResponse({ 'username_valid': True})
 
 def register(request):

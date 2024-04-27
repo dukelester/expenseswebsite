284128cdc181ac1ad44b2fd939d262ddde4d714f
Duke Nyamongo (Lester) <dukelester4@gmail.com>
2024-04-27 14:47:32 +0300

    ,Created a view to validate the email using the validate_email package

--- authentication/views.py
+++ authentication/views.py
@@ -2,9 +2,24 @@ from django.shortcuts import render
 from django.views import View
 from django.http import JsonResponse
 from django.contrib.auth.models import User
+from validate_email import validate_email
+
 import json
 
 # Create your views here.
+class EmailValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        email = data['email']
+        if not validate_email(email):
+            return JsonResponse({
+                'email_error': 'The email is invalid. Enter a valid email',
+            }, status=400)
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({
+                'email_error': 'Invalid email, choose another email'
+                }, status=409)
+        return JsonResponse({ 'email_valid': True})
 
 class UsernameValidationView(View):
     def post(self, request):

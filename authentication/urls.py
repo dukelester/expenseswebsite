3dab1e92255daf9e584e540297ce6f3655884761
Duke Nyamongo (Lester) <dukelester4@gmail.com>
2024-04-27 14:53:51 +0300

    Updated the urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,6 +5,9 @@ from . import views
 
 urlpatterns = [
     path('register', views.register, name='register'),
-    path('validate_username', csrf_exempt(views.UsernameValidationView.as_view()), name='validate_username'),
+    path('validate_username', csrf_exempt(views.UsernameValidationView.as_view()),
+        name='validate_username'),
+    path('validate_email', csrf_exempt(views.EmailValidationView.as_view()),
+        name='validate_email'),
     path('login', views.login, name='login'),
 ]

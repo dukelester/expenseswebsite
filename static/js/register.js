834a1588eed4f57b1bfb779c0d813d3f48874e5d
Duke Nyamongo (Lester) <dukelester4@gmail.com>
2024-04-27 15:18:52 +0300

    Added the email validation

--- static/js/register.js
+++ static/js/register.js
@@ -1,21 +1,44 @@
 
 const usernameField = document.querySelector('#usernameField');
+const emailField = document.querySelector('#email');
 const invalidFeedBackArea = document.querySelector('.invalid_feedback');
+const inavlidEmail = document.querySelector('.invalid_feedback_email');
+
 usernameField.classList.remove('is-invalid');
 invalidFeedBackArea.style.display = 'none';
+emailField.classList.remove('is-invalid');
+inavlidEmail.style.display = 'none';
 // add eventy listener - keyup
 usernameField.addEventListener('keyup', (e) => {
-    usernameValue = e.target.value;
-    console.log(usernameValue);
+    const usernameValue = e.target.value;
+    // console.log(usernameValue);
     if (usernameValue) {
         fetch('/authentication/validate_username', {
             body: JSON.stringify({ username: usernameValue }), method: 'POST'
         }).then(res => res.json()).then(data => {
-            console.log(data);
+            // console.log(data);
             if (data.username_error){
                 usernameField.classList.add('is-invalid');
                 invalidFeedBackArea.style.display = 'block';
                 invalidFeedBackArea.innerHTML = `<p>${data.username_error}</p>`
+            }
+        });
+    }
+});
+
+// add eventy listener - keyup on the email
+emailField.addEventListener('keyup', (e) => {
+    const emailValue = e.target.value;
+    console.log(emailValue);
+    if (emailValue) {
+        fetch('/authentication/validate_email',{
+            body: JSON.stringify({ email: emailValue }), method: 'POST'
+        }).then(res => res.json()).then(data => {
+            console.log(data);
+            if(data.email_error) {
+                emailField.classList.add('is-invalid');
+                invalidFeedBackArea.style.display = 'block';
+                invalidFeedBackArea.innerHTML = `<p>${data.email_error}</p>`
             }
         });
     }

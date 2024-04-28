a971f135287c3598e88a1439f610198954125deb
Duke Nyamongo (Lester) <dukelester4@gmail.com>
2024-04-29 00:49:06 +0300

    Read the data and added it to a list of key and value

--- userpreferences/views.py
+++ userpreferences/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-
+import os
+import json
+from django.conf import settings
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'preferences/index.html')
+    curreny_data = []
+    file_path = os.path.join(settings.BASE_DIR, 'currencies.json')
+    # import pdb; pdb.set_trace()
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+        for key, value in data.items():
+            curreny_data.append({'name': key, 'value': value})
+    return render(request, 'preferences/index.html', { 'currencies': curreny_data })

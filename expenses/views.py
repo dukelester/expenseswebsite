c88297eae812d55f46715faaadb2a10368f29b7d
Duke Nyamongo (Lester) <dukelester4@gmail.com>
2024-04-28 23:34:04 +0300

    added restrictions for logged in users and unlogged in ones to access the pages for expenses

--- expenses/views.py
+++ expenses/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='authentication/login')
 def index(request):
     return render(request, 'expenses/index.html')
 
+@login_required(login_url='authentication/login')
 def add_expense(request):
     return render(request, 'expenses/add_expense.html')

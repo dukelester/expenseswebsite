df784f34e692d53581d0f23c32c078afe996966d
Duke Nyamongo (Lester) <dukelester4@gmail.com>
2024-04-28 23:41:20 +0300

    Renamed the app to userpreferences

--- expenseswebsite/settings.py
+++ expenseswebsite/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django_password_validators.password_history',
     'expenses',
     'authentication',
+    
 ]
 
 MIDDLEWARE = [
--- userpreferences/apps.py
+++ userpreferences/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class UserprefencesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "userprefences"
+    name = "userpreferences"

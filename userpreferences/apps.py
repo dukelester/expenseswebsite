bfe5af84d4e872801bed4827adae83443d1d9fb4
Duke Nyamongo (Lester) <dukelester4@gmail.com>
2024-04-28 23:42:44 +0300

    Renamed the app to userpreferences

--- userpreferences/apps.py
+++ userpreferences/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UserprefencesConfig(AppConfig):
+class UserpreferencesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "userpreferences"

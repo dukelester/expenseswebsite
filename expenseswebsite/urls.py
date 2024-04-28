63267801a0fa71f01d866ca0a7fe76f08a1bffe8
Duke Nyamongo (Lester) <dukelester4@gmail.com>
2024-04-29 00:19:30 +0300

    Added the preferences simple view, and urls mappings

--- expenseswebsite/urls.py
+++ expenseswebsite/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('expenses.urls')),
     path('authentication/', include('authentication.urls')),
+    path('preferences', include('userpreferences.urls')),
 ]

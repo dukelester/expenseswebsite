f57072d1b5d7818dad4a5cc9a77652cf76f49d29
Duke Nyamongo (Lester) <dukelester4@gmail.com>
2024-05-01 16:40:11 +0300

    Hide the pagination while searching

--- static/js/searchExpenses.js
+++ static/js/searchExpenses.js
@@ -1,12 +1,24 @@
 const searchField = document.querySelector('#searchField');
+const outputTable = document.querySelector('.output');
+outputTable.style.display = 'none'
+
+const expensesTable = document.querySelector('.expensesTable');
+const pagination = document.querySelector('.pagination-container');
 
 searchField.addEventListener('keyup', (e) => {
     const searchValue = e.target.value;
     if (searchValue.trim()) {
+        pagination.style.display = 'none';
         fetch('/search_expense', {body: JSON.stringify({searchText: searchValue})
         , method: "POST"
     }).then((res) => res.json()).then((data) => {
-        console.log(data);
+        expensesTable.style.display = 'none';
+        if (data.length === 0){
+            outputTable.innerHTML = '<p> There is no results for you!</p>'
+        } else {
+            outputTable.style.display = 'block';
+            
+        }
     });
     }
 });

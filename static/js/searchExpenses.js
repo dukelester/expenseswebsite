f04857a92b469c032f0344854cf5e71bc6805cbf
Duke Nyamongo (Lester) <dukelester4@gmail.com>
2024-05-01 15:38:32 +0300

    Added the JS code for data fetching

--- static/js/searchExpenses.js
+++ static/js/searchExpenses.js
@@ -2,7 +2,11 @@ const searchField = document.querySelector('#searchField');
 
 searchField.addEventListener('keyup', (e) => {
     const searchValue = e.target.value;
-    if (searchValue) {
-        console.log(searchValue);
+    if (searchValue.trim()) {
+        fetch('expenses/search_expense', {body: JSON.stringify({searchText: searchValue})
+        , method: "POST"
+    }).then((res) => res.json()).then((data) => {
+        console.log(data);
+    });
     }
 });

75ca8751100efb661d91d8c1604369b2b1ea942b
Duke Nyamongo (Lester) <dukelester4@gmail.com>
2024-05-02 01:08:12 +0300

    added the models to the admin section

--- userincome/admin.py
+++ userincome/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import SourceOfIncome, UserIncome
+
+admin.site.register(SourceOfIncome)
+admin.site.register(UserIncome)

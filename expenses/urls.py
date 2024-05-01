ebfe713c975da9071b4ae8b38f8d8de6e1d54af0
Duke Nyamongo (Lester) <dukelester4@gmail.com>
2024-05-01 15:40:13 +0300

    Added the url mapping for the searching

--- expenses/urls.py
+++ expenses/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('add_expense', views.add_expense, name='add_expense'),
     path('edit_expense/<int:expense_id>', views.edit_expense, name='edit_expense'),
     path('delete_expense/<int:expense_id>', views.delete_expense, name='delete_expense'),
+    path('search_expense', views.search_expenses, name='search_expense'),
 ]
